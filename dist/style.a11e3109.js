0b8556e19ded9580d0a86cf968cf0ef4fcb521a6
LrMgic <63558214+LrMgic@users.noreply.github.com>
2021-04-16 10:17:31 +0200

    modif1

--- dist/style.a11e3109.js
+++ dist/style.a11e3109.js
@@ -122,7 +122,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\img\\variedades-de-cerveza.jpg":[["variedades-de-cerveza.49deded1.jpg","assets/img/variedades-de-cerveza.jpg"],"assets/img/variedades-de-cerveza.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58914" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60503" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

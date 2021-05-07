75de902cb6a3826631f456642898f69fa182a4bc
LrMgic <63558214+LrMgic@users.noreply.github.com>
2021-05-07 18:02:50 +0200

    V1.0.1

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
+    "gifsicle": { "optimizationLevel": 1, "interlaced": false, "colors": 10 },
     "mozjpeg": { "progressive": true, "quality": 10 },
     "pngquant": { "quality": [0.25, 0.5] },
     "svgo": {

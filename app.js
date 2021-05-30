82f4f118493503c3e64d358142458f5659f08399
LrMgic <63558214+LrMgic@users.noreply.github.com>
2021-05-30 19:20:29 +0200

    PEC 3 V3.2.0

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const sharp = require("sharp");
 const nom = "fachadavideo";
 const nom2 = nom;
 const termi = "png";
+const termi2 = "webp";
 
-/* sharp("assets/img/"+nom+"."+termi).resize(320, undefined).toFile("assets/img/"+nom2+"-320."+termi); */
-sharp("assets/img/"+nom+"."+termi).resize(480, undefined).toFile("assets/img/"+nom2+"-480."+termi);
-/* sharp("assets/img/"+nom+"."+termi).resize(600, undefined).toFile("assets/img/"+nom2+"-600."+termi); */
-/* sharp("assets/img/"+nom+"."+termi).resize(800, undefined).toFile("assets/img/"+nom2+"-800."+termi); */
+/* sharp("assets/img/"+nom+"."+termi).resize(320, undefined).toFile("assets/img/"+nom2+"-320."+termi2); */
+sharp("assets/img/"+nom+"."+termi).resize(480, undefined).toFile("assets/img/"+nom2+"-480."+termi2);
+/* sharp("assets/img/"+nom+"."+termi).resize(600, undefined).toFile("assets/img/"+nom2+"-600."+termi2); */
+/* sharp("assets/img/"+nom+"."+termi).resize(800, undefined).toFile("assets/img/"+nom2+"-800."+termi2); */

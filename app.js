47f0d2b0e545924f340db5cb61282bd4370df962
LrMgic <63558214+LrMgic@users.noreply.github.com>
2021-05-30 18:52:59 +0200

    PEC 3 V3.2.0

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const sharp = require("sharp");
 
-const nom = "cervezas";
+const nom = "variedades-de-cerveza";
 const nom2 = nom;
-const termi = "webp";
+const termi = "jpg";
 
 sharp("assets/img/"+nom+"."+termi).resize(320, undefined).toFile("assets/img/"+nom2+"-320."+termi);
 sharp("assets/img/"+nom+"."+termi).resize(480, undefined).toFile("assets/img/"+nom2+"-480."+termi);
 sharp("assets/img/"+nom+"."+termi).resize(600, undefined).toFile("assets/img/"+nom2+"-600."+termi);
+sharp("assets/img/"+nom+"."+termi).resize(800, undefined).toFile("assets/img/"+nom2+"-800."+termi);

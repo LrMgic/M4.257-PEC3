c3e616b3cdfcacd5e174c09b344fc663543806f0
LrMgic <63558214+LrMgic@users.noreply.github.com>
2021-05-30 18:58:11 +0200

    PEC 3 V3.2.0

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const sharp = require("sharp");
 
-const nom = "variedades-de-cerveza";
+const nom = "fachadavideo";
 const nom2 = nom;
-const termi = "jpg";
+const termi = "png";
 
-sharp("assets/img/"+nom+"."+termi).resize(320, undefined).toFile("assets/img/"+nom2+"-320."+termi);
+/* sharp("assets/img/"+nom+"."+termi).resize(320, undefined).toFile("assets/img/"+nom2+"-320."+termi); */
 sharp("assets/img/"+nom+"."+termi).resize(480, undefined).toFile("assets/img/"+nom2+"-480."+termi);
-sharp("assets/img/"+nom+"."+termi).resize(600, undefined).toFile("assets/img/"+nom2+"-600."+termi);
-sharp("assets/img/"+nom+"."+termi).resize(800, undefined).toFile("assets/img/"+nom2+"-800."+termi);
+/* sharp("assets/img/"+nom+"."+termi).resize(600, undefined).toFile("assets/img/"+nom2+"-600."+termi); */
+/* sharp("assets/img/"+nom+"."+termi).resize(800, undefined).toFile("assets/img/"+nom2+"-800."+termi); */
